6ce641a2c072f914589e87889a152648c7a47632
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-03-26 05:06:05 +0200

    created crud commands

--- src/app/components/cars/shared/car.service.ts
+++ src/app/components/cars/shared/car.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import {Car} from './car.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
+  carList: AngularFireList<any>;
+  selectedCar: Car = new Car();
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getData() {
+    this.carList = this.firebase.list('cars');
+    return this.carList;
+  }
+
+  insertCar(car: Car) {
+    this.carList.push({
+      name: car.name,
+      model: car.model,
+      year: car.year,
+      engine: car.engine,
+      engineType: car.engineType,
+      kilometrage: car.kilometrage,
+      options: car.options
+    });
+  }
+
+  updateCar(car: Car) {
+     this.carList.update(car.$key,
+       {
+         name: car.name,
+         model: car.model,
+         year: car.year,
+         engine: car.engine,
+         engineType: car.engineType,
+         kilometrage: car.kilometrage,
+         options: car.options
+     });
+  }
+
+  deleteCar($key: string) {
+    this.carList.remove($key);
+  }
 }
