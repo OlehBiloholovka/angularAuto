221f0aab436870f3976a6a7a498b0173fd47a7c1
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-03-26 09:40:53 +0200

    add reset button action

--- src/app/components/cars/car/car.component.ts
+++ src/app/components/cars/car/car.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from '../shared/car.service';
+import { NgForm } from '@angular/forms';
+import {modelGroupProvider} from '@angular/forms/src/directives/ng_model_group';
 
 @Component({
   selector: 'app-car',
@@ -11,6 +13,23 @@ export class CarComponent implements OnInit {
   constructor(private carService: CarService) { }
 
   ngOnInit() {
+    this.onResetForm();
   }
 
+  onSubmit(carForm: NgForm) {
+  }
+
+  onResetForm(carForm: NgForm) {
+    if (carForm != null) carForm.reset();
+    this.carService.selectedCar = {
+      $key: null,
+      name: '',
+      model: '',
+      year: 2000,
+      engine: 0.0,
+      engineType: '',
+      mileage: 10000,
+      options: []
+    };
+  }
 }
