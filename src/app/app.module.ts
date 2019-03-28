d4b585c84ab550691c48bcb6baec307b274d3b5d
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-03-28 18:01:27 +0200

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AngularFireModule} from '@angular/fire';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import { FormsModule } from '@angular/forms';
 import {ToastrModule} from 'ngx-toastr';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {environment} from '../environments/environment';
@@ -11,6 +12,12 @@ import { CarComponent } from './components/cars/car/car.component';
 import { CarsComponent } from './components/cars/cars.component';
 import { CarListComponent } from './components/cars/car-list/car-list.component';
 import { HeaderComponent } from './components/header/header.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CarsComponent},
+  {path: 'cars', component: CarListComponent},
+  {path: 'car', component: CarComponent}
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +32,8 @@ import { HeaderComponent } from './components/header/header.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
     FormsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
