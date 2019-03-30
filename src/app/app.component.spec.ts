d16f737875abc55e283b70d773f605bf223c02cb
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-03-30 22:05:13 +0200

    changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularAuto'`, () => {
+  it(`should have as title 'carUsell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularAuto');
+    expect(app.title).toEqual('carUsellcarUsell');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularAuto!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to carUsellcarUsell!');
   });
 });
